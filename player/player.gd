76b598e671d5a89f77be82a65105e710bae0d3b3
daniboygg <danigayosog@gmail.com>
2025-04-06 22:24:47 +0200

    Modify player to have 2 lights on the helmet

--- player/player.gd
+++ player/player.gd
@@ -2,9 +2,9 @@ extends CharacterBody2D
 
 signal sonar_activated(charges: int)
 
-@onready var point_light_2d: PointLight2D = $PointLight2D
 @onready var light_increase_timer: Timer = $LightIncreaseTimer
 @onready var light_decrease_timer: Timer = $LightDecreaseTimer
+@onready var helmet_light: PointLight2D = $HelmetLight
 
 @onready var coyote_timer: Timer = $CoyoteTimer
 var can_trigger_coyote_time := true
@@ -32,23 +32,24 @@ var MIN_ENERGY := 0.3
 var MAX_ENERGY := 1
 var BURST_ENERGY := 1.5
 
+var light_x_position := 0
+
 func _ready():
-	point_light_2d.visible = true
-	point_light_2d.texture_scale = light_scale
-	point_light_2d.energy = MAX_ENERGY
-	light_increase_timer.wait_time = light_increasee_time
-	
+	$ColorRect.visible = false
+	light_x_position = helmet_light.position.x
 	set_charges()
 	
 	
 func _process(delta: float) -> void:
-	increase_sonar_size(delta)	
+	pass
 
 
-func _draw():
-	if not light_increase_timer.is_stopped() or not light_decrease_timer.is_stopped():
-		draw_circle(point_light_2d.position, circle_radius, Color.WHITE, false, 1.5)
-		draw_circle(point_light_2d.position, circle_radius * 0.8, Color.WHITE, false, 1.25)
+#func _draw():
+	#if not light_increase_timer.is_stopped() or not light_decrease_timer.is_stopped():
+		#draw_circle(point_light_2d.position, circle_radius, Color.WHITE, false, 1.5)
+		#draw_circle(point_light_2d.position, circle_radius * 0.8, Color.WHITE, false, 1.25)
+	#var tween = get_tree().create_tween()
+	#tween.tween_property(point_light_2d, "energy", target_energy, 2)
 
 
 func _physics_process(delta: float) -> void:
@@ -78,8 +79,12 @@ func _physics_process(delta: float) -> void:
 		
 	if velocity.x < 0:
 		$Sprite2D.flip_h = true
+		helmet_light.position.x = -light_x_position
+		helmet_light.scale.x = -1
 	elif velocity.x > 0:
 		$Sprite2D.flip_h = false
+		helmet_light.position.x = light_x_position
+		helmet_light.scale.x = 1
 
 	move_and_slide()
 	
@@ -90,55 +95,15 @@ func is_on_coyote_floor():
 	
 func _input(event):
 	if event is InputEventMouseButton:
-		if event.button_index == MOUSE_BUTTON_LEFT:
-			if event.pressed and light_decrease_timer.is_stopped():
-				enable_sonar()
+		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+				pass
+				#enable_sonar()
 				
-		if event.button_index == MOUSE_BUTTON_RIGHT:
-			if event.pressed and light_decrease_timer.is_stopped():
+		if event.button_index == MOUSE_BUTTON_RIGHT and event.pressed:
 				# cheat!
 				charges += 1
 				set_charges()
-				
 
-func enable_sonar():
-	charges = clamp(charges - 1, MIN_CHARGES, MAX_CHARGES)
-	
-	point_light_2d.energy = 1.5
-	var target_energy = remap(charges, MIN_CHARGES, MAX_CHARGES, MIN_ENERGY, MAX_ENERGY)
-	print(target_energy)
-	
-	var tween = get_tree().create_tween()
-	tween.tween_property(point_light_2d, "energy", target_energy, 2)
-	
-	set_charges()
-	light_increase_timer.start()
-	sonar_activated.emit(charges)
-
-
-func increase_sonar_size(delta):
-	if not light_increase_timer.is_stopped():
-		var prev := point_light_2d.texture_scale
-		point_light_2d.texture_scale += light_increase_speed * delta
-		
-		var factor_change := (point_light_2d.texture_scale - prev)  / prev 
-		circle_radius = circle_radius * (1 + factor_change)
-		queue_redraw()
-		
-	
-func disable_sonar():
-	point_light_2d.texture_scale = min_light_scale
-	circle_radius = INITIAL_CIRCLE_RADIUS
-	queue_redraw()
-	
 
 func set_charges():
 	label.text = "%s" % charges
-
-
-func _on_light_increase_timer_timeout() -> void:
-	light_decrease_timer.start()
-
-
-func _on_light_decrease_timer_timeout() -> void:
-	disable_sonar()
